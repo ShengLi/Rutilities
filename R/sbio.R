c98efdf6b74a599cfb0c0fd0bac16934fab8beab
Sheng Li <Shelly1436@gmail.com>
2011-11-23 01:51:52 -0500

    Add sBio function for source biocLite.R script

---
+++ R/sbio.R
@@ -0,0 +1,18 @@
+#' Source biocLite.R from bioconductor.R
+#'
+#' Source biocLite.R from bioconductor.R/biocLite.R
+#' 
+#' @title source biocLite.R
+#' @examples
+#' sBio()
+#'
+#' @rdname sBio 
+#' @export sBio
+sBio = function(){
+    source("http://bioconductor.org/biocLite.R")
+}
+
+#' @return \code{NULL}
+
+
+
